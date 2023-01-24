f43a0ef757bc1635a3dcd509eb881f9de3eb2c57
angelgaeta <an.redjdal@gmail.com>
2023-01-24 11:36:03 +0100

    dashboard esprit jeu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -38,7 +38,7 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Gameboard');
+            ->setTitle('Esprit Jeu');
     }
 
     public function configureMenuItems(): iterable
