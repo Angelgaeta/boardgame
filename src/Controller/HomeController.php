97811d4e6f76c5f5ab79e4297818527250b21ed3
angelgaeta <an.redjdal@gmail.com>
2023-02-08 10:04:24 +0100

    refresh

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Game;
+use App\Repository\GameRepository;
+use App\Form\GameType;
+
+
 
 class HomeController extends AbstractController
 {
+
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(GameRepository $repo): Response
     {
+        $games = $repo->findAll();
+        dump($games);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'GameController',
+            'games' => $games
         ]);
     }
 }
+
+
+
